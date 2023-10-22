7fbcc246772fbfa3f61c88e50eaba25a34b902c4
Eric Mesa <ericsbinaryworld@gmail.com>
2023-10-21 20:41:37 -0400

    agent button updates things now

--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"image/color"
 	"log"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	api "www.github.com/djotaku/spacetraders_go/spacetradersapi"
 )
 
-func updateAgentArea(thisContentBox *fyne.Container) *fyne.Container {
-	factionListSelection := widget.NewSelect([]string{"Option 1", "Option 2"}, func(value string) {
+func updateAgentArea(thisContentBox *fyne.Container) {
+	factionListSelection := widget.NewSelect([]string{"COSMIC", "VOID", "GALACTIC", "QUANTUM", "DOMINION", "ASTRO",
+		"CORSAIRS", "OBSIDIAN", "AEGIS", "UNITED", "SOLITARY", "COBALT", "OMEGA", "ECHO", "LORDS", "CULT", "ANCIENTS", "SHADOW", "ETHEREAL"}, func(value string) {
 		log.Println("Select set to", value)
 	})
 	callsignInput := widget.NewEntry()
 	callsignInput.SetPlaceHolder("Enter your Call Sign")
 	newAgentButton := widget.NewButton("Get a New Agent", func() { api.RegisterNewAgent(factionListSelection.Selected, callsignInput.Text) })
-	thisContentBox = container.New(layout.NewHBoxLayout(), factionListSelection, newAgentButton)
+	thisContentBox.RemoveAll()
+	thisContentBox.Add(factionListSelection)
+	thisContentBox.Add(callsignInput)
+	thisContentBox.Add(newAgentButton)
 	thisContentBox.Refresh()
-	return thisContentBox
 }
 
 func main() {
@@ -43,10 +44,19 @@ func main() {
 	spaceTradersApp := app.New()
 	appWindow := spaceTradersApp.NewWindow("Space Traders")
 	tempContentText := widget.NewLabel("Click a button to change")
-	contentBox := container.New(layout.NewHBoxLayout(), tempContentText)
-	tempTopRowText := canvas.NewText("Top Row", color.White)
-	topRowBox := container.New(layout.NewHBoxLayout(), tempTopRowText)
-	agentButton := widget.NewButton("Agents", func() { contentBox = updateAgentArea(contentBox); contentBox.Refresh() })
+	contentBox := container.New(layout.NewVBoxLayout(), tempContentText)
+	agentName := widget.NewLabel("Agent: ")
+	agentNameValue := widget.NewLabel("") // needs data binding
+	money := widget.NewLabel("Money: ")
+	moneyValue := widget.NewLabel("") // needs data binding
+	numberOfShips := widget.NewLabel("Number of Ships: ")
+	numberOfShipsValue := widget.NewLabel("") // needs data binding
+	activeContracts := widget.NewLabel("Active Contracts: ")
+	activeContractsValue := widget.NewLabel("") // needs data binding
+	timeToNextMove := widget.NewLabel("Time to Next Move: ")
+	timeToNextMoveValue := widget.NewLabel("") // needs data binding
+	topRowBox := container.New(layout.NewHBoxLayout(), agentName, agentNameValue, money, moneyValue, numberOfShips, numberOfShipsValue, activeContracts, activeContractsValue, timeToNextMove, timeToNextMoveValue)
+	agentButton := widget.NewButton("Agents", func() { updateAgentArea(contentBox) })
 	shipButton := widget.NewButton("Ships", func() { log.Println("Ships") })
 	contractButton := widget.NewButton("Contracts", func() { log.Println("Contracts") })
 	waypointsButton := widget.NewButton("Waypoints", func() { log.Println("Waypoints") })
